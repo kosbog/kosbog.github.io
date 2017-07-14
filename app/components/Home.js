1f9042e50a9e32c0b6fa43866e74d1ba2089944c
kosbog <johnkot@alterpost.org>
2017-07-14 18:08:50 +0300

    WIP

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,47 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Video from './Video';
+import Bio from './Bio';
 import header from '../assets/images/header.jpg';
 
 const Header = () => {
     return (
         <header></header>
-    )
-}
-
-const Bio = () => {
-    return (
-        <div className="bio">
-            <div className="bio-about">
-                <span className="title">
-                    about me
-                </span>
-                <div className="text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus illo aperiam,
-                        debitis pariatur neque magnam culpa voluptatibus, nemo aliquid deleniti adipisci,
-                        temporibus eum dicta sit odit dolorum consequatur sequi? Veritatis.
-                </div>
-                <div className="icons">
-                    <ul>
-                        <li>
-                            <i className="icon icon-html"></i>
-                            <span>HTML</span>
-                        </li>
-                        <li>
-                            <i className="icon icon-css"></i>
-                            <span>CSS</span>
-
-                        </li>
-                        <li>
-                            <i className="icon icon-js"></i>
-                            <span>JavaScript</span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className="bio-image">qq</div>
-            <div className="bio-contacts">qq</div>
-        </div>
     )
 }
 
@@ -69,13 +34,7 @@ const Info = () => {
     )
 }
 
-const FullName = () => {
-    return (
-        <div className="fullname">
 
-        </div>
-    )
-}
 
 class Home extends Component {
     render() {
