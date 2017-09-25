e7fc212493d42a5943302e2ba11dc90340d92b3f
kosbog <johnkot@alterpost.org>
2017-09-25 16:05:51 +0300

    restart

--- app/components/Experience.js
+++ app/components/Experience.js
@@ -4,55 +4,7 @@ class Experience extends Component {
     render() {
         return (
             <div className="experience">
-                <div className="experience-title">
-                    experience
-                </div>
-                <div className="experience-content">
-                    <div className="place">
-                        <div className="years">
-                            <p>
-                                2017
-                        </p>
-                        </div>
-                        <div className="position">
-                            <p>
-                                "Company Name"
-                            </p>
-                            <span>
-                                Front-end Developer
-                            </span>
-                        </div>
-                        <div className="description">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aliquid eligendi veniam, nihil eum sapiente quod, architecto mollitia facere,
-                            minima suscipit quos facilis possimus aliquam incidunt assumenda voluptatibus at eaque? Blanditiis.
-                        </p>
-                        </div>
-                    </div>
-                    <div className="place">
-                       <div className="years">
-                            <p>
-                                2017
-                        </p>
-                        </div>
-                        <div className="position">
-                            <p>
-                                "Company Name"
-                            </p>
-                            <span>
-                                Front-end Developer
-                            </span>
-                        </div>
-                        <div className="description">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aliquid eligendi veniam, nihil eum sapiente quod, architecto mollitia facere,
-                            minima suscipit quos facilis possimus aliquam incidunt assumenda voluptatibus at eaque? Blanditiis.
-                        </p>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         );
     }
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,42 +1,10 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Video from './Video';
-import Bio from './Bio';
-import Experience from './Experience';
-import header from '../assets/images/header.jpg';
-
-const Header = () => {
-    return (
-        <header></header>
-    )
-}
-
-const Education = () => {
-    return (
-        <div className="education">
-
-        </div>
-    )
-}
-
-const Info = () => {
-    return (
-        <div className="education">
-
-        </div>
-    )
-}
 
 class Home extends Component {
     render() {
         return (
-            <div className="content">
-                {/*<Video />*/}
-                <Header />
-                <Bio />
-                <Experience />
-                <Education />
-                <Info />
+            <div>
+                
             </div>
         );
     }
